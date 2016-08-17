1294efe19a8fb5a1c785d623ce53cda3d6be5b0f
Simone <simone.scalco93@gmail.com>
2016-08-17 16:57:57 +0200

    Column division for dataset 'localita'

--- run.sh
+++ run.sh
@@ -15,7 +15,9 @@ mkdir results/insert_queries
 mkdir results/insert_queries/raw
 
 # Parses the CSV file and specifies the column division
-python parseCSV.py 6,83,98,104,116,147 R04_indicatori_2011_areecensimento.csv
+#python parseCSV.py 6,83,98,104,116,147 R04_indicatori_2011_areecensimento.csv
+
+python parseCSV.py 6,13,89,104,110,122,153 R04_indicatori_2011_localita.csv
 
 # Merges the files contained in the first subdirectory
 python mergeFiles.py results/create_tables/ .sql
