56dc5c016e89dec905d81071ae30137dc67c6ebb
StefanoTavonatti <stefanotavonatti93@gmail.com>
2016-08-17 17:35:17 +0200

    lines_sezioni

--- run.sh
+++ run.sh
@@ -17,9 +17,9 @@ mkdir results/insert_queries/raw
 # Parses the CSV file and specifies the column division
 #python parseCSV.py 6,83,98,104,116,147 R04_indicatori_2011_areecensimento.csv
 
-python parseCSV.py 6,13,89,104,110,122,153 R04_indicatori_2011_localita.csv
+#python parseCSV.py 6,13,89,104,110,122,153 R04_indicatori_2011_localita.csv
 
-#python parseCSV.py 6,13,89,104,110,122,153 R04_indicatori_2011_sezioni.csv
+python parseCSV.py 6,86,101,107,119,150, R04_indicatori_2011_sezioni.csv
 
 # Merges the files contained in the first subdirectory
 python mergeFiles.py results/create_tables/ .sql
