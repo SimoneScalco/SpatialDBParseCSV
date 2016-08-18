5c9843f8721ea125d1c980cb84a057174c639d67
StefanoTavonatti <stefanotavonatti93@gmail.com>
2016-08-18 16:34:51 +0200

    basic remap

---
+++ remap.py
@@ -0,0 +1,25 @@
+import sys
+import copy
+
+arguments=copy.copy(sys.argv)
+fileName=arguments[1]
+
+del arguments[0]
+del arguments[0]
+
+inputFile=open(fileName,'r')
+lines=inputFile.readlines()
+inputFile.close()
+
+for argument in arguments:
+    oldName=str(argument).split(':')[0]
+    newName=str(argument).split(':')[1]
+    for i in range(0,len(lines)):
+        lines[i]=lines[i].replace(oldName,newName)
+
+outputFile=open(fileName,'w')
+
+for line in lines:
+    outputFile.write(line)
+
+outputFile.close()
