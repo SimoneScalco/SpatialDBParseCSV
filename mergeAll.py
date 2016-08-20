4bfbe511d9d8e99a775dde9436d3376f7032c691
Simone <simone.scalco93@gmail.com>
2016-08-20 17:43:47 +0200

    mergeAll.py readjusted

--- mergeAll.py
+++ mergeAll.py
@@ -1,22 +1,34 @@
-outputFile=open('results/DB.sql','w')
+
+# Output file handler
+outputFileName = 'results/DB.sql'
+outputFile=open(outputFileName,'w')
+
+# First input file handler (create tables directory)
 inputFile=open("results/create_tables/DIRECTORY_MERGE.sql","r")
 
-print '[STATUS 5] merging DIRECTORY_MERGE files...'
+print '[STATUS 5] Merging DIRECTORY_MERGE files...'
 
+# Gets all the lines from the input file
 lines=inputFile.readlines()
 
+# Writes all the data just read to the output file
 for line in lines:
     outputFile.write(line)
 
 inputFile.close()
 
+
+# Second input file handler (insert queries directory)
 inputFile=open('results/insert_queries/DIRECTORY_MERGE.sql','r')
+
+# Gets all the lines from the input file
 lines=inputFile.readlines()
 
+# Writes all the data just read to the output file
 for line in lines:
     outputFile.write(line)
 
 inputFile.close()
 outputFile.close()
 
-print '[STATUS 5] done'
+print '[STATUS 5] All local merged files have been copied entirely in ' + outputFileName
