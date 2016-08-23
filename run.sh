3bdc18dd993b9a352fdb530aa7b5f08454d92a7b
Simone <simone.scalco93@gmail.com>
2016-08-23 18:39:15 +0200

    Main script editing

--- run.sh
+++ run.sh
@@ -19,9 +19,6 @@ mkdir results/insert_queries/raw
 # Parses the CSV file and specifies the column division
 python parseCSV.py R04_indicatori_2011_areecensimento.csv 6,6,-1 83,7_6,6 98,7_6,6 104,7_6,6 116,7_6,6 147,7_6,6
 
-
-#python parseCSV.py R04_indicatori_2011_sezioni.csv 6, 87, 102, 108, 120, 151
-
 # Merges the files contained in the first subdirectory
 python mergeFiles.py results/create_tables/ .sql
 
