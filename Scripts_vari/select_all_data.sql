879441aa04d8560075327745172d74bc9d89f279
Simone <simone.scalco93@gmail.com>
2016-08-26 10:02:55 +0200

    New joint table added

--- Scripts_vari/select_all_data.sql
+++ Scripts_vari/select_all_data.sql
@@ -6,3 +6,9 @@ FROM comuni NATURAL JOIN localita NATURAL JOIN popolazione_residente NATURAL JOI
 
 SELECT *
 FROM comuni NATURAL JOIN sezioni NATURAL JOIN popolazione_residente NATURAL JOIN stranieri_residenti NATURAL JOIN abitazioni NATURAL JOIN famiglie NATURAL JOIN edifici
+
+
+SELECT *
+FROM (comuni NATURAL JOIN localita NATURAL JOIN popolazione_residente NATURAL JOIN stranieri_residenti NATURAL JOIN abitazioni NATURAL JOIN famiglie NATURAL JOIN edifici), loc_extra
+WHERE comuni.procom = loc_extra.pro_com
+LIMIT 500
