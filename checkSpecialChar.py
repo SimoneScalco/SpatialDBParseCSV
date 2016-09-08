297574a1da6d4c4ed4b257d7c2f83fdb93f8b0ce
Simone <simone.scalco93@gmail.com>
2016-09-08 15:57:29 +0200

    checkSpecialChar commented

--- checkSpecialChar.py
+++ checkSpecialChar.py
@@ -1,6 +1,8 @@
 
+# Dictionary of characters
 charachters={}
 
+# Opens the INSERT_ALL file and reads every line, then appends the content in the dictionary
 with open('results/INSERT_ALL.sql','r') as file:
     for line in file:
         for c in line:
@@ -9,9 +11,12 @@ with open('results/INSERT_ALL.sql','r') as file:
 
 print charachters
 
+# Opens the output file
 outFile=open('results/chars.txt','w')
 
+# For each character in the dictionary, write it in the output file
 for k in charachters:
     outFile.write(str(k)+'\n')
 
+# Closes the output file
 outFile.close()
